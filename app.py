cf0a43f033dde5bc87a4118b1b844844cd387469
Mayank Maurya <51482587+MAYANKTIGER@users.noreply.github.com>
2023-12-06 18:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    return "CI-CD pipeline is running successfully......."
+    return "CI-CD pipeline is running successfully.......!!!!!"
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0",port=5000)
